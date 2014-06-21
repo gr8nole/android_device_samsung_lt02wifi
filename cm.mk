d0546f079e7e838ed17c801fc5000dfe4316b8dd
Dave <davemartinfsu@gmail.com>
2014-06-21 15:46:17 -0400

    remove calling/mms apps

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+#$(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
 
